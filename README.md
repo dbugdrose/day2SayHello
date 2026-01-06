f6b2b0272e62d8deb394d9580e5eca3d25a168fe
dbugdrose <darcy.ophelia.rose@gmail.com>
2026-01-06 13:12:25 -0800

    SayHelloAPI

---
+++ README.md
@@ -0,0 +1,9 @@
+// Darcy Rose
+
+ // Date Revised: 1/6/26
+
+ // Say Hello 
+
+ // Created an API that takes in a name using the get step and returns a greeting with the user's name.
+
+// Peer Review: Pallavi Pattanashetti - 
---
+++ sayHello/Controllers/SayHelloController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sayHello.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SayHelloController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("Name")]
+        public string Name(string userName)
+        {
+            return $"Hello, {userName}!";
+        }
+    }
+}
